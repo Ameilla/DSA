b7df59ba9c40eff38eb6647c20c89a9c7f3b47a3
Amar Dwarakacherala <73354017+Ameilla@users.noreply.github.com>
2024-01-22 23:23:50 +0530

    Create max_sum_subarray.java

---
+++ src/Arrays/max_sum_subarray.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class max_sum_subarray {
+	public static void main(String[] args)
+	{
+		int a[]= {-2,-5,6,-1,-3};
+		System.out.print(maxsubarray(a));
+	}
+
+	private static int maxsubarray(int[] a) {
+		int max=a[0];
+		int cmax=a[0];
+		for(int i=1;i<a.length;i++)
+		{
+			cmax=Math.max(a[i],a[i]+cmax);
+			max=Math.max(max, cmax);
+		}
+		
+		return max;
+	}
+
+}
