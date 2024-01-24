5249d92a40d23c7a5ffb416914553ae6856450a3
Amar Dwarakacherala <73354017+Ameilla@users.noreply.github.com>
2024-01-24 19:14:43 +0530

    Update max_sum_subarray.java

--- src/Arrays/max_sum_subarray.java
+++ src/Arrays/max_sum_subarray.java
@@ -4,7 +4,23 @@ public class max_sum_subarray {
 	public static void main(String[] args)
 	{
 		int a[]= {-2,-5,6,-1,-3};
-		System.out.print(maxsubarray(a));
+		System.out.print(one(a));
+	}
+	public static int one(int[] a)
+	{
+		int max=0;
+		for(int i=0;i<a.length;i++)
+		{
+			int csum=0;
+			for(int j=i;j<a.length;j++)
+			{
+				csum+=a[j];
+				if(max<csum)
+					max=csum;
+			}
+			
+		}
+		return max;
 	}
 	private static int maxsubarray(int[] a) {
 		int max=a[0];
