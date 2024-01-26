437e8aa982dc826ca12aace7ffde8a06b2f12083
Amar Dwarakacherala <73354017+Ameilla@users.noreply.github.com>
2024-01-26 19:15:01 +0530

    Create Binary_Search.java

---
+++ src/Recursion/Binary_Search.java
@@ -0,0 +1,23 @@
+package Recursion;
+
+public class Binary_Search {
+
+	public static int search(int[] a,int key,int start,int end)
+	{
+		if(start>end)
+			return -1;
+		int mid = start+(end-start)/2;
+		if(a[mid]==key)
+			return mid;
+		if(a[mid]>key)
+			return search(a,key,start,mid-1);
+		return search(a,key,mid+1,end);
+	}
+	public static void main(String[] args)
+	{
+		int a[]= {1,2,3,5,7,20,25};
+		int key = 25;
+		int i = search(a,key,0,a.length);
+		System.out.print(i);
+	}
+}
