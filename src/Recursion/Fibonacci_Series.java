bf2774d8dbfbdb5bfaa8b0103bf396e54ab740a0
Amar Dwarakacherala <73354017+Ameilla@users.noreply.github.com>
2024-01-26 18:48:58 +0530

    Create Fibonacci_Series.java

---
+++ src/Recursion/Fibonacci_Series.java
@@ -0,0 +1,27 @@
+package Recursion;
+import java.util.*;
+public class Fibonacci_Series {
+	public static int fib(int n)
+	{
+		if(n==0)
+		{
+			System.out.println(0);
+			return 0;
+		}
+		if(n==1)
+		{
+			System.out.println(1);
+			return 1;
+		}
+		System.out.println(fib(n-1)+fib(n-2));
+		return fib(n-1)+fib(n-2);
+	}
+	public static void  main(String[] args)
+	{
+		int n=4;
+		ArrayList<Integer> t = new ArrayList<>();
+		for(int i=0;i<n;i++)
+			t.add(fib(i));
+	}
+
+}
