884a61ded2698a527dc685119ea1f783b45c4e72
Amar Dwarakacherala <73354017+Ameilla@users.noreply.github.com>
2024-01-26 19:00:23 +0530

    Update Linear_Search.java

--- src/Recursion/Linear_Search.java
+++ src/Recursion/Linear_Search.java
@@ -1,5 +1,24 @@
 package Recursion;
 
 public class Linear_Search {
+	public static int search(int a[],int key,int index)
+	{
+		if(a.length<=index)
+			return -1;
+		if(a[index]==key)
+			return index;
+		return search(a,key,index+1);
+	}
+	
+	public static void main(String[] args)
+	{
+		int[] a = {1,4,5,7,8,10};
+		int key=10;
+		int index = search(a,key,0);
+		if(index!=-1)
+			System.out.print(key+"="+index);
+		else
+			System.out.print("Not found");
+	}
 
 }
