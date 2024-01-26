572e095a0d826f76e6a8974f55bbd415502163cd
Amar Dwarakacherala <73354017+Ameilla@users.noreply.github.com>
2024-01-26 14:15:48 +0530

    Create Prime_Numbers.java

---
+++ src/Prime_Numbers.java
@@ -0,0 +1,46 @@
+import java.util.*;
+public class Prime_Numbers {
+	public static boolean isprime(int n)
+	{
+		for(int i=2;i<Math.sqrt(n);i++)
+		{
+			if(n%i==0)
+				return false;
+		}
+		return true;
+	}
+	public static HashSet<Integer> seiveprime(int n)
+	{
+		HashSet<Integer> prime = new HashSet<>();
+		
+		for(int i=2;i<=n;i++)
+		{
+			if(isprime(i))
+			{
+				for(int j=i*2;j<=n;j=j+i)
+				{
+					prime.add(j);
+				}
+			}
+		}
+//		for(int i=2;i<=n;i++)
+//		{
+//			if(!prime.contains(i))
+//				System.out.println(i);
+//		}
+//		for(int Integer: prime)
+//			System.out.print(Integer);
+//		
+		return prime;
+		
+	}
+	public static void main(String[] args)
+	{
+		seiveprime(15);
+		HashSet<Integer> p = seiveprime(15);
+		for(int Integer: p)
+			System.out.print("\n"+Integer);
+		
+	}
+
+}
