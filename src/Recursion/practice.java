8065481ecaef1d655346cae029507cbad4d743e2
Amar Dwarakacherala <73354017+Ameilla@users.noreply.github.com>
2024-01-26 19:46:10 +0530

    Update practice.java

--- src/Recursion/practice.java
+++ src/Recursion/practice.java
@@ -1,16 +1,40 @@
 package Recursion;
 
 public class practice {
-	static void print(int n)
+	static int print(int n)
 	{
-		if(n==5)
+		if(n==1)
+			return 1;
+		return n*print(n-1);
+		
+	}
+	static int sum_of_digit(int n)
+	{
+		if(n==0)
+			return 0;
+		return (n%10)+sum_of_digit(n/10);
+	}
+	static int product(int n)
+	{
+		if(n%10==n)
+			return n;
+		return (n%10)*product(n/10);
+	}
+	static int rev=0;
+	static void reverse(int n)
+	{
+		if(n==0)
 			return;
-		System.out.print(n);
-		print(n+1);
+		int rem=n%10;
+		rev=rev*10+rem;
+		reverse(n/10);
+		
 	}
 	public static void main(String[] args)
 	{
-		print(1);
+		int n=1234;
+		reverse(n);
+		System.out.print(rev);
 	}
 
 }
