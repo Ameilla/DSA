c34d0ca2f359dcae58f828e3f25fc82065b4aaa6
Amar Dwarakacherala <amarkkr369@gmail.com>
2024-01-28 23:18:46 +0530

    Update trash.java

--- src/trash.java
+++ src/trash.java
@@ -2,7 +2,23 @@
 public class trash {
 	public static void main(String[] args)
 	{
-		System.out.print(24);
+		int n=5;
+		System.out.print(n+" ");
+		while(n>1)
+		{
+			if(n%2==0)
+			{
+				n=(int) Math.sqrt(n);
+				System.out.print(n+" ");
+			}
+			else
+			{
+//				System.out.print(n);
+				double t =Math.sqrt(n)*Math.sqrt(n)*Math.sqrt(n);
+				n = (int)t;
+				System.out.print(n+" ");
+			}
+		}
 	}
 
 }
