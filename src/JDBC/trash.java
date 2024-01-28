1dd03ef0c6a111c3cc8e7f3404faa15c1e8ecd3d
Amar Dwarakacherala <amarkkr369@gmail.com>
2024-01-28 17:39:11 +0530

    Update trash.java

--- src/JDBC/trash.java
+++ src/JDBC/trash.java
@@ -1,5 +1,88 @@
 package JDBC;
+import java.sql.*;
+import java.util.*;
+
 
 public class trash {
+	static Scanner input = new Scanner(System.in);
+	public static class DB
+	{
+		public static void createDB(String name)
+		{
+			try {
+				String url = "jdbc:mysql://localhost:3306/moca";
+				String userName = "root";
+				String password = "";
+//				Class.forName("oracle.jdbc.driver.OracleDriver");
+//				Class.forName("com.mysql.cj.jdbc.Driver");
+				Connection conn = DriverManager.getConnection(url,userName,password);
+				Statement st = conn.createStatement();
+				System.out.print("Connected");
+				String query = "create database "+name;
+				boolean bl = st.execute(query);
+				System.out.print(bl);
+				conn.close();
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+		public static void createTable(String name)
+		{
+			try {
+				String url = "jdbc:mysql://localhost:3306/Amari";
+				String userName = "root";
+				String password = "";
+//				Class.forName("oracle.jdbc.driver.OracleDriver");
+//				Class.forName("com.mysql.cj.jdbc.Driver");
+				Connection conn = DriverManager.getConnection(url,userName,password);
+				Statement st = conn.createStatement();
+				System.out.print("Connected");
+				String query = "create table "+name+" (rollno int(3))";
+				boolean bl = st.execute(query);
+				System.out.print(bl);
+				conn.close();
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+		public void createData()
+		{
+			try {
+				String url = "jdbc:mysql://localhost:3306/";
+				String db = "Amari";
+				String userName = "root";
+				String password = "";
+//				Class.forName("oracle.jdbc.driver.OracleDriver");
+//				Class.forName("com.mysql.cj.jdbc.Driver");
+				Connection conn = DriverManager.getConnection(url+db,userName,password);
+				Statement st = conn.createStatement();
+				int x=input.nextInt();
+				System.out.print("Connected");
+				String query = "Insert into Amar (rollno) values(x)";
+				boolean bl = st.execute(query);
+				System.out.print(bl);
+				conn.close();
+			}
+			catch(Exception e)
+			{
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	public static void main(String[] args)
+	{
+		DB db = new DB();
+//		db.createDB("Harsh");
+		db.createData();
+		
+		
+		
+	}
+	
 
 }
