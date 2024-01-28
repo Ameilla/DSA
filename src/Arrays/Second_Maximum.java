116c74310876edc39ec89c6416469cfc7f6b19fb
Amar Dwarakacherala <amarkkr369@gmail.com>
2024-01-28 17:49:05 +0530

    Create Second_Maximum.java

---
+++ src/Arrays/Second_Maximum.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class Second_Maximum {
+	public static void main(String[] args)
+	{
+		int[] a = {1,3,4,216,19,17};
+		int max1 = Integer.MIN_VALUE;
+		int max2 = Integer.MIN_VALUE;
+		for(int i=0;i<a.length;i++)
+		{
+			if(a[i]>max1)
+			{
+				max2=max1;
+				max1=a[i];
+			}
+			else if(a[i]>max2 && a[i]!=max1)
+			{
+				max2=a[i];
+			}
+		}
+		System.out.print(max1 +" "+max2);
+	}
+
+}
