7840a8bd38e53a1608553d41369657d40cfba5e0
Amar Dwarakacherala <amarkkr369@gmail.com>
2024-01-29 17:00:55 +0530

    Update Second_Maximum.java

--- src/Arrays/Second_Maximum.java
+++ src/Arrays/Second_Maximum.java
@@ -3,17 +3,17 @@ package Arrays;
 public class Second_Maximum {
 	public static void main(String[] args)
 	{
-		int[] a = {1,3,4,216,19,17};
-		int max1 = Integer.MIN_VALUE;
-		int max2 = Integer.MIN_VALUE;
+		int[] a = {1,3,4,1,1,216,19,17};
+		int max1 = Integer.MAX_VALUE;
+		int max2 = Integer.MAX_VALUE;
 		for(int i=0;i<a.length;i++)
 		{
-			if(a[i]>max1)
+			if(a[i]<max1)
 			{
 				max2=max1;
 				max1=a[i];
 			}
-			else if(a[i]>max2 && a[i]!=max1)
+			else if(a[i]<max2 && a[i]!=max1)
 			{
 				max2=a[i];
 			}
