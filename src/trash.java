be7b6d639d5cf992b2ea338cddd10e19324cbbee
Amar Dwarakacherala <amarkkr369@gmail.com>
2024-01-29 19:43:59 +0530

    Update trash.java

--- src/trash.java
+++ src/trash.java
@@ -1,24 +1,46 @@
-
+import java.io.*;
+import java.util.*; 
 public class trash {
-	public static void main(String[] args)
-	{
-		int n=5;
-		System.out.print(n+" ");
-		while(n>1)
-		{
-			if(n%2==0)
-			{
-				n=(int) Math.sqrt(n);
-				System.out.print(n+" ");
-			}
-			else
-			{
-//				System.out.print(n);
-				double t =Math.sqrt(n)*Math.sqrt(n)*Math.sqrt(n);
-				n = (int)t;
-				System.out.print(n+" ");
-			}
-		}
-	}
 
+    public static void main (String[] args) {
+        Scanner s=new Scanner(System.in);
+        String str=s.next();
+        int n=str.length();
+        int i;
+        StringBuilder a=new StringBuilder(str);
+        a.setLength(1);
+        int q=n/16;
+        int r=n%16;
+        if(r<10)
+        {
+        	a.append(String.valueOf(r));
+        }
+        else if(r==10)
+        {
+        	a.append("a");
+        }
+        else if(r==11)
+        {
+        	a.append("b");
+        }
+        else if(r==12)
+        {
+        	a.append("c");
+        }
+        else if(r==13)
+        {
+        	a.append("d");
+        }
+        else if(r==14)
+        {
+        	a.append("e");
+        }
+        else if(r==15)
+        {
+        	a.append("5");
+        }
+        System.out.print(a.reverse().toString());
+        
+        
+    }
 }
