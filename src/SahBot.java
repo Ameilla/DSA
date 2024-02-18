93d2c28722b93d8e537edb714ed248a8e259ce03
Amar Dwarakacherala <amarkkr369@gmail.com>
2024-02-18 07:56:59 +0530

    Update SahBot.java

--- src/SahBot.java
+++ src/SahBot.java
@@ -1,17 +1,39 @@
+
+import java.util.*;
 public class SahBot {
     public static void main(String[] args) {
-        String[] a = {"Apple", "Amar", "Banana"};
-        for (int i = 0; i < a.length; i++) {
-            for (int j = i + 1; j < a.length; j++) {
-                if (a[i].compareTo(a[j]) > 0) {
-                    String temp = a[i];
-                    a[i] = a[j];
-                    a[j] = temp;
-                }
-            }
-        }
-        for (String s : a) {
-            System.out.println(s);
-        }
+//        String[] a = {"Apple", "Amar", "Banana"};
+//        for (int i = 0; i < a.length; i++) {
+//            for (int j = i + 1; j < a.length; j++) {
+//                if (a[i].compareTo(a[j]) > 0) {
+//                    String temp = a[i];
+//                    a[i] = a[j];
+//                    a[j] = temp;
+//                }
+//            }
+//        }
+//        for (String s : a) {
+//            System.out.println(s);
+//        }
+    	Scanner input = new Scanner(System.in);
+    	int n=5;
+    	ArrayList<Integer> arr = new ArrayList<>();
+    	for(int i=0;i<n;i++)
+    	{
+    		int x = input.nextInt();
+    		arr.add(x);
+    	}
+    	System.out.print(arr);
+    	for(int i=0;i<n;i++)
+    	{
+    		int x = input.nextInt();
+    		arr.add(x);
+    	}
+    	System.out.print(arr);
+    	
+    	
     }
 }
+
+
+
