4115e793cce0126c1d82ffb4a73dc86169741f9a
Amar Dwarakacherala <amarkkr369@gmail.com>
2024-02-21 22:46:26 +0530

    Update t1.java

--- src/t1.java
+++ src/t1.java
@@ -2,23 +2,12 @@ import java.util.*;
 public class t1 {
 	public static void main(String[] args)
 	{
-		int b=12;
-		float c=0.0f;
-		double d=0.7;
-//		int res=a+b;
-//		System.out.print("result: "+res);
-		
-		  Scanner input = new Scanner(System.in);
-		  System.out.print("Enter a");
-	      int a=input.nextInt();
-	      System.out.print("Enter f");
-	      float f=input.nextFloat();
-	      System.out.print("Enter d");
-	      double df=input.nextDouble();
-	      System.out.print("Enter char\n1.Addition\n2.Sub");
-	      char ch=input.next().charAt(0);
-	      System.out.print(ch);
-		
+		int t=5;
+        for(int i=5+1;i<=2147483647;i++)
+        {
+            t=t&i;
+        }
+       System.out.print(t);	
 	}
 
 }
