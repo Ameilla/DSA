c3220b89b1f46c2fef84f29fd1ac30c7b997c57b
Amar Dwarakacherala <amarkkr369@gmail.com>
2024-02-23 13:14:41 +0530

    Update t1.java

--- src/t1.java
+++ src/t1.java
@@ -1,13 +1,26 @@
 import java.util.*;
 public class t1 {
+	public static int findJudge(int n, int[][] trust) {
+        int[] count = new int[n+1];
+        for(int[] i : trust)
+        {
+            count[i[0]]--;
+            count[i[1]]++;
+        }
+        for(int i=1;i<=n;i++)
+        {
+            if(count[i]==n-1)
+                return i;
+        }
+        return -1;
+        
+    }
 	public static void main(String[] args)
 	{
-		int t=5;
-        for(int i=5+1;i<=2147483647;i++)
-        {
-            t=t&i;
-        }
-       System.out.print(t);	
+		int[][] x = {{1,2},{2,2},{3,2}};
+		System.out.print(findJudge(3,x));
+		
 	}
+	
 
 }
