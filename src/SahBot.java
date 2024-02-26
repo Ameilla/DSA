e1c4073caf75ffd036b24fc26824713c849794d0
Amar Dwarakacherala <amarkkr369@gmail.com>
2024-02-26 10:15:20 +0530

    Update SahBot.java

--- src/SahBot.java
+++ src/SahBot.java
@@ -1,6 +1,22 @@
 
 import java.util.*;
 public class SahBot {
+	public static int find(int a[])
+	{
+		ArrayList<Integer> pos = new ArrayList<>();
+		ArrayList<Integer> neg = new ArrayList<>();
+		for(int i=0;i<a.length;i++)
+		{
+			if(i%2==0)
+				pos.add(a[i]);
+			else
+				neg.add(a[i]);
+		}
+		Collections.sort(pos);
+		Collections.sort(neg);
+		return pos.get(pos.size()-2)+neg.get(1);
+		
+	}
     public static void main(String[] args) {
 //        String[] a = {"Apple", "Amar", "Banana"};
 //        for (int i = 0; i < a.length; i++) {
@@ -15,22 +31,8 @@ public class SahBot {
 //        for (String s : a) {
 //            System.out.println(s);
 //        }
-    	Scanner input = new Scanner(System.in);
-    	int n=5;
-    	ArrayList<Integer> arr = new ArrayList<>();
-    	for(int i=0;i<n;i++)
-    	{
-    		int x = input.nextInt();
-    		arr.add(x);
-    	}
-    	System.out.print(arr);
-    	for(int i=0;i<n;i++)
-    	{
-    		int x = input.nextInt();
-    		arr.add(x);
-    	}
-    	System.out.print(arr);
-    	
+    	int a[] = {3,2,1,7,5,4};
+    	System.out.print(find(a));
     	
     }
 }
